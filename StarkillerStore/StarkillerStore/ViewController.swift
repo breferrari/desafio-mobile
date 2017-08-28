5f0b8d9fe42b9fc1cbc7eda1591905ca1fd2988e
Brenno Ferrari <brennoferrari@gmail.com>
2017-08-28 18:53:52 -0300

    Initial commit

---
+++ StarkillerStore/StarkillerStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarkillerStore
+//
+//  Created by Brenno Ferrari on 27/08/17.
+//
+//
+
+import UIKit
+
+class ViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setStarkillerStoreTitle()
+    }
+
+}
+
